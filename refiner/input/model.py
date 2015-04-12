a76af4b83244e3945a570f1deabad1bbf71263f7
Max Spencer <mrmaxspencer@googlemail.com>
2015-04-13 00:43:47 +0100

    Created refiner.input.pdftohtml module.
    Copied code for parsing output of pdftohtml into a new module. In the
    future other input.* modules could be added to refining output of
    other conversion/OCR programs.

---
+++ refiner/input/model.py
@@ -0,0 +1,94 @@
+import itertools
+
+from refiner import core
+
+
+class Text(object):
+    def __init__(self, string, page, left, top, width = 0, height = 0, right = None, bottom = None, font = None):
+        self.string = string
+        self.box = core.Box(
+            left, top,
+            width=width, height=height,
+            page=page,
+            right=right, bottom=bottom
+        )
+        self.font = font
+
+    def __str__(self):
+        return str(self.string)
+
+    @property
+    def page(self):
+        return self.box.page
+    
+    @property
+    def left(self):
+        return self.box.left
+
+    @property
+    def top(self):
+        return self.box.top
+
+    @property
+    def width(self):
+        return self.box.width
+
+    @property
+    def height(self):
+        return self.box.height
+
+    @property
+    def right(self):
+        return self.box.right
+
+    @property
+    def bottom(self):
+        return self.box.bottom
+
+
+class Font(object):
+    def __init__(self, id, family, size, color):
+        '''Create a new Font instance.
+
+        size may be an int or float, or the string representation of either of
+        these.
+
+        '''
+        self.id = id
+        self.family = family
+        self.color = color
+        
+        if type(size) == int or type(size) == float:
+            self.size = size
+        elif type(size) == str:
+            try:
+                self.size = int(size)
+            except ValueError:
+                try:
+                    self.size = float(size)
+                except ValueError:
+                    raise ValueError('Could not parse size as int or float')
+        else:
+            raise TypeError('size must be an int, float or str')
+        
+    def __str__(self):
+        return '<Font {}>'.format(self.id)
+
+
+class Page(object):
+    def __init__(self, number, width, height):
+        '''Create a new Page instance.'''
+        self.number = int(number)
+        self.width = width
+        self.height = height
+        self.texts = list()
+
+    def __str__(self):
+        return '<Page {}>'.format(self.number)
+
+class Document(object):
+    def __init__(self):
+        '''Create a new Document instance.'''
+        self.pages = list()
+        self.fonts = dict()
+        
---
+++ refiner/input/pdftohtml.py
@@ -0,0 +1,55 @@
+import subprocess
+import re
+import tempfile
+import bs4
+import sys
+
+from refiner.input.model import Document, Page, Font, Text
+
+
+def parse(string, replacements = []):
+    for r in replacements:
+        string = re.sub(r[0], r[1], string)
+    
+    soup = bs4.BeautifulSoup(string)
+    document = Document()
+
+    fontspec_elements = soup.find_all('fontspec')
+    for e in fontspec_elements:
+        font = Font(e['id'], e['family'], e['size'], e['color'])
+        document.fonts[font.id] = font
+
+    page_elements = soup.find_all('page')
+    for e in page_elements:
+        page = Page(int(e['number']), int(e['width']), int(e['height']))
+        document.pages.append(page)
+
+        for te in e.find_all('text'):
+            string = ''.join(te.strings)
+            text = Text(
+                string,
+                page,
+                int(te['left']),
+                int(te['top']),
+                int(te['width']),
+                int(te['height']),
+                font=document.fonts.get(te['font'], None)
+            )
+            page.texts.append(text)
+
+    return document
+
+
+def parse_file(path):
+    with tempfile.NamedTemporaryFile(mode='w+', suffix='.xml') as xml_file:
+        args = ['pdftohtml', '-xml', path, xml_file.name]
+        subprocess.check_call(args)
+        xml = xml_file.read()
+    return parse(xml)
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        d = parse(f.read())
+    for t in d.pages[3].texts:
+        print((str(t.box), str(t)))
