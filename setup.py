082ad5f209aee43bf7ce9c61e5b502bdbfcb1174
Max Spencer <mrmaxspencer@googlemail.com>
2015-04-21 23:16:53 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = 'pdfrefiner',
+    packages = ['refiner', 'refiner.input', 'refiner.output'],
+    version = '0.1',
+    description = 'A python module for improving the output of PDF parsing and conversion tools.',
+    author = 'Max Spencer',
+    author_email = 'mrmaxspencer@gmail.com',
+    url = 'https://github.com/maxspencer/pdfrefiner',
+    download_url = 'https://github.com/maxspencer/pdfrefiner/tarball/0.1',
+    keywords = ['pdf', 'document'],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.4'
+    ],
+)
